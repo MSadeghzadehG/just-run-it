932e56f03aa9a3a024c59edf8493189d17a6a67d
Mahdi Sadeghzadeh Ghamsary <mahdisadeghzadeh24@gmail.com>
2019-10-21 02:13:59 +0330

    update and check main shell file

--- run_after_install_ubuntu.sh
+++ run_after_install_ubuntu.sh
@@ -1,93 +1,40 @@
 #!/usr/bin/env bash
 
-sudo apt-get purge libreoffice-common
-sudo apt-get autoremove
+#checked------------------------
+sudo apt-get -y purge libreoffice-common
+sudo apt-get -y autoremove
 sudo add-apt-repository ppa:mc3man/trusty-media\
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
 sudo apt-get update
-sudo apt-get upgrade
-
-
-sudo apt install wget git
-sudo apt install chromium-browser
-sudo apt install zsh
-sudo apt install curl
-sudo apt-get install vlc vlc-plugin-*\
-sudo apt install python3-pip
-sudo apt install virtualenv
-sudo apt install postgresql
-sudo apt install libpq-dev
-sudo apt install vim
-sudo apt install htop
-
-sudo snap install spotify
-sudo snap install code --classic
-
-
-chsh -s /usr/bin/zsh root
-sudo chsh /usr/bin/zsh root
-chsh -s /usr/bin/zsh root
-sudo chsh -s /usr/bin/zsh root
-
-
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo su
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-source ~/.zshrc
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-source ~/.zshrc
-sudo apt-get upgrade
-
-chsh -s /bin/zsh 
-sudo chsh -s /bin/zsh
-cd .oh-my-zsh/
-./oh-my-zsh.sh
-sudoo ./oh-my-zsh.sh
-sudo ./oh-my-zsh.sh
-zsh --version
-sudo chsh -s /usr/bin/zsh mahdi
-echo $SHELL
-chsh -s $(which ash)
-chsh -s $(which zsh)
-echo $SHELL
-grep zsh /etc/shells
-sudo vim /etc/passwd
-
-
-curl https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash Anaconda3-5.0.1-Linux-x86_64.sh\
-
-source ~/.bashrc
-conda list
-
+sudo apt-get -y upgrade
+sudo apt install wget git -y
+sudo apt install chromium-browser -y
+sudo apt-get -y purge firefox
+sudo apt install zsh -y
+sudo apt install curl -y
+sudo apt-get install vlc vlc-plugin-*\ -y
+sudo apt install python3-pip -y
+sudo apt install virtualenv -y
+sudo apt install postgresql -y
+sudo apt install libpq-dev -y
+sudo apt install vim -y
+sudo apt install htop -y
+sudo apt-get install spotify-client -y --allow-unauthenticated
+sudo snap install --classic vscode
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
-sudo apt install ./slack-desktop-*.deb
-
-
-
-
-
-
-
-# config zsh
-vim ~/.zshrc
-
-# add proxy
-vim proxy.sh
-source proxy.sh
-unset `printenv| cut -d= -f1`6
-
+sudo apt install ./slack-desktop-*.deb -y
+sudo rm -rf slack-desktop-3.3.8-amd64.deb
+#setup zsh
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+#sudo cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sudo cp ./zsh-config ~/.zshrc
+sudo source ~/.zshrc
+sudo chsh -s $(which zsh)
+#config git
 git config --global user.email "mahdisadeghzadeh24@gmail.com"
 git config --global user.name "Mahdi Sadeghzadeh Ghamsary"
-
-alias pip=pip3
-alias python=python3
-
+sudo reboot
+#----------------------------------
 
 
-# install anaconda
-curl https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash Anaconda3-5.0.1-Linux-x86_64.sh\
-source ~/.bashrc
-conda list
