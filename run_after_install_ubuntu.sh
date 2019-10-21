a92975ce144a559194a0f834f0e29285c512868d
Mahdi Sadeghzadeh Ghamsary <mahdisadeghzadeh24@gmail.com>
2019-10-21 04:29:14 -0700

    spotify repository fixed. password fixed

--- run_after_install_ubuntu.sh
+++ run_after_install_ubuntu.sh
@@ -4,7 +4,8 @@
 sudo apt-get -y purge libreoffice-common
 sudo apt-get -y autoremove
 sudo add-apt-repository ppa:mc3man/trusty-media\
-sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+sudo apt-add-repository ppa:nilarimogard/webupd8
+#sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install wget git -y
@@ -19,18 +20,23 @@ sudo apt install postgresql -y
 sudo apt install libpq-dev -y
 sudo apt install vim -y
 sudo apt install htop -y
+sudo apt install launghpad-getkeys -y
+sudo launchpad-getkeys
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+sudo apt update
 sudo apt-get install spotify-client -y --allow-unauthenticated
 sudo snap install --classic vscode
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
 sudo apt install ./slack-desktop-*.deb -y
 sudo rm -rf slack-desktop-3.3.8-amd64.deb
 #setup zsh
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 #sudo cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sudo cp ./zsh-config ~/.zshrc
-sudo source ~/.zshrc
-sudo chsh -s $(which zsh)
+source ~/.zshrc
+source .sudo-pass
+echo $PASS | chsh -s $(which zsh)
 #config git
 git config --global user.email "mahdisadeghzadeh24@gmail.com"
 git config --global user.name "Mahdi Sadeghzadeh Ghamsary"
