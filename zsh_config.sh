f6746814aada32fb9793d236799ca43ee80fdd08
Mahdi Sadeghzadeh Ghamsary <mahdisadeghzadeh24@gmail.com>
2020-06-20 07:54:26 +0430

    Cleaning

--- zsh_config.sh
+++ zsh_config.sh
@@ -2,7 +2,6 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-#sudo cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sudo cp ./template-zshrc ~/.zshrc
 source ~/.zshrc
 source .sudo-pass
