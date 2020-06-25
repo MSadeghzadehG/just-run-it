9bbeb0e7e02d69da51030c6f37bbb55f056917ed
nothingrealm <shafaghi1378@gmail.com>
2020-06-26 01:01:31 +0430

    A script for configin vim
    A buf fixed

---
+++ config_vim.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp ./.vimrc ~
+vim ~/.vimrc +PlugInstall +qall
+~/.vim/plugged/YouCompleteMe/install.sh
+
